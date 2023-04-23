ab0e21a0bec941de8361f773e3fa757c65cd187c
Дима <@DESKTOP-TC925PR>
2023-04-23 14:16:52 +0300

    Add project files.

---
+++ dz 12.04/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace dz12_04
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        string path = "C:\\";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                path = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string[] astrFiles = Directory.GetFiles(path);
+            listBox1.Items.Add("Всего файлов: " + astrFiles.Length);
+            foreach (string file in astrFiles)
+            {
+                if (file.Contains("." + textBox1.Text))
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+
+        }
+    }
+}
